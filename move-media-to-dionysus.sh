89d377dc47b8d2e764b4e643b93bb18e89827d1b
Arnaud Verstuyf <arnaud.verstuyf@togetheranywhere.com>
2021-06-30 14:22:14 -0700

    Improve media sync

--- move-media-to-dionysus.sh
+++ move-media-to-dionysus.sh
@@ -1,2 +1,21 @@
-rsync -r -e ssh --progress --remove-source-files --exclude-from='exclude-on-move.txt' ~/Downloads/plex/ arnaud@192.168.86.11:/srv/media/        
-find ~/Downloads/plex/ -depth -type d -empty -delete
+#!/bin/sh
+VERSION="2"
+
+SOURCES=(
+    $HOME/downloads/plex/
+    $HOME/downloads/bits/plex/
+    )
+
+for source in "${SOURCES[@]}"; do
+    # Guard against the source directory not existing
+    [ ! -d "$source" ] && continue
+
+    echo "Processing, source: $source"
+    
+    # Move the source files
+    # Note: Directories will be copied instead of moved
+    rsync -r -e ssh --progress --remove-source-files --exclude-from='exclude-on-move.txt' "$source" arnaud@192.168.86.11:/srv/media/        
+
+    # Delete the empty source directories
+    find "$source" -depth -type d -empty -delete
+done
